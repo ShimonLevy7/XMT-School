18153541e87fa579cd4f4053d3346e04625f622a
Mami Tomoe <159314988+ShimonLevy7@users.noreply.github.com>
2024-03-07 18:27:30 +0200

    removed old code

--- Frontend/Student Website/xmt_school_student_react_ts/src/ts/Users.ts
+++ Frontend/Student Website/xmt_school_student_react_ts/src/ts/Users.ts
@@ -3,40 +3,44 @@ import { GetCookie, SetCookie, TOKEN_KEY } from '../utils/Cookies';
 import Database from './Remote';
 
 // Types and constants
-const DefaultAvatarUrl: string = 'https://cdn.discordapp.com/attachments/836004673299021824/1107722671850004570/avatar.png';
+const DefaultAvatarUrl: string =
+	'https://cdn.discordapp.com/attachments/836004673299021824/1107722671850004570/avatar.png';
 
 export enum UserTypes {
 	Guest = 0,
 	Student = 1,
 	Teacher = 2,
-	Administrator = 100
+	Administrator = 100,
 }
 
-export const UserTypeNames : { [id: number]: string } = {
+export const UserTypeNames: { [id: number]: string } = {
 	[UserTypes.Guest]: 'Guest',
 	[UserTypes.Student]: 'Student',
 	[UserTypes.Teacher]: 'Teacher',
-	[UserTypes.Administrator]: 'Administrator'
-}
+	[UserTypes.Administrator]: 'Administrator',
+};
 
 export interface IUser {
-	id: number,
-	type: UserTypes,
-	username: string,
-	email: string,
-	avatarUrl: string|undefined
+	id: number;
+	type: UserTypes;
+	username: string;
+	email: string;
+	avatarUrl: string | undefined;
 }
 
-export class Guest implements IUser
-{
+export class Guest implements IUser {
 	id: number;
 	type: UserTypes;
 	username: string;
 	email: string;
 	avatarUrl: string;
 
-	constructor(id: number, username: string, email: string, avatarUrl: string = DefaultAvatarUrl)
-	{
+	constructor(
+		id: number,
+		username: string,
+		email: string,
+		avatarUrl: string = DefaultAvatarUrl
+	) {
 		this.id = id;
 		this.type = UserTypes.Guest;
 		this.username = username;
@@ -45,16 +49,19 @@ export class Guest implements IUser
 	}
 }
 
-export class Student implements IUser
-{
+export class Student implements IUser {
 	id: number;
 	type: UserTypes;
 	username: string;
 	email: string;
 	avatarUrl: string;
 
-	constructor(id: number, username: string, email: string, avatarUrl: string = DefaultAvatarUrl)
-	{
+	constructor(
+		id: number,
+		username: string,
+		email: string,
+		avatarUrl: string = DefaultAvatarUrl
+	) {
 		this.id = id;
 		this.type = UserTypes.Student;
 		this.username = username;
@@ -63,16 +70,19 @@ export class Student implements IUser
 	}
 }
 
-export class Teacher implements IUser
-{
+export class Teacher implements IUser {
 	id: number;
 	type: UserTypes;
 	username: string;
 	email: string;
 	avatarUrl: string;
 
-	constructor(id: number, username: string, email: string, avatarUrl: string = DefaultAvatarUrl)
-	{
+	constructor(
+		id: number,
+		username: string,
+		email: string,
+		avatarUrl: string = DefaultAvatarUrl
+	) {
 		this.id = id;
 		this.type = UserTypes.Teacher;
 		this.username = username;
@@ -81,16 +91,19 @@ export class Teacher implements IUser
 	}
 }
 
-export class Administrator implements IUser
-{
+export class Administrator implements IUser {
 	id: number;
 	type: UserTypes;
 	username: string;
 	email: string;
 	avatarUrl: string;
 
-	constructor(id: number, username: string, email: string, avatarUrl: string = DefaultAvatarUrl)
-	{
+	constructor(
+		id: number,
+		username: string,
+		email: string,
+		avatarUrl: string = DefaultAvatarUrl
+	) {
 		this.id = id;
 		this.type = UserTypes.Administrator;
 		this.username = username;
@@ -106,58 +119,53 @@ export let CurrentUserData: IUser = DefaultUser;
 export let CurrentUserToken: string = '';
 
 // Functions
-export const setCurrentUserData = function(userData: IUser)
-{
+export const setCurrentUserData = function (userData: IUser) {
 	CurrentUserData = userData;
-}
+};
 
-export const getCurrentUserAsync = async function()
-{
-	let user: string | IUserResult = await Database.getByKeyAsync('user', [ CurrentUserToken ]);
+export const getCurrentUserAsync = async function () {
+	let user: string | IUserResult = await Database.getByKeyAsync('user', [
+		CurrentUserToken,
+	]);
 
-	if (typeof(user) === 'string' || user.message.messageType === ServerMessageTypes.Error)
+	if (
+		typeof user === 'string' ||
+		user.message.messageType === ServerMessageTypes.Error
+	)
 		return DefaultUser;
 
 	return user.data;
-}
+};
 
-export const matchesMinUserType = function(level: number, levels: number|object|null)
-{
-	if (levels === null)
-		return true;
+export const matchesMinUserType = function (
+	level: number,
+	levels: number | object | null
+) {
+	if (levels === null) return true;
 
-	if (typeof levels === 'number' && levels >= level)
-		return true;
+	if (typeof levels === 'number' && levels >= level) return true;
 
-	if (Array.isArray(levels) && levels.includes(level))
-		return true;
+	if (Array.isArray(levels) && levels.includes(level)) return true;
 
 	return false;
-}
+};
 
-export const isLoggedInAsync = async function()
-{
+export const isLoggedInAsync = async function () {
 	const currentUser = await getCurrentUserAsync();
 
-	if (currentUser.type === UserTypes.Guest)
-		return false;
+	if (currentUser.type === UserTypes.Guest) return false;
 
 	return true;
-}
+};
 
-export const reloadCurrentUserToken = function()
-{
+export const reloadCurrentUserToken = function () {
 	CurrentUserToken = GetCookie(TOKEN_KEY) ?? '';
+};
 
-	//LocalStorage.readFromLocalStorage(TOKEN_KEY) || '';
-}
-
-export const saveCurrentUserToken = function(token: string)
-{
+export const saveCurrentUserToken = function (token: string) {
 	SetCookie(TOKEN_KEY, token, 7);
-}
+};
 
-export const deleteCurrentUserToken = function()
-{
+export const deleteCurrentUserToken = function () {
 	SetCookie(TOKEN_KEY, '', 0);
-}
+};
